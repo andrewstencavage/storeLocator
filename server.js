b5596eeaa109d388e3bf145e52f37ce90c4a8cfa
Andrew <astencavage@gmail.com>
2020-01-14 08:20:15 -0500

    docker works with mongodb

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.use(express.static(path.join(__dirname,'public')));
 //Routes
 app.use('/api/v1/stores', require('./routes/stores'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
